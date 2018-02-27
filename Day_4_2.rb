1d6d80f3b6fa2f0a94f6e914684ece69718e905f
GabbyMont <gemo1236@gmail.com>
2018-02-27 01:29:44 -0500

    Formatting continued

---
+++ Day_4_2.rb
@@ -0,0 +1,6 @@
+star_wars_seven = "Jar Jar Binks Han Solo Luke lens flare"
+				#  0123456789...
+puts star_wars_seven[14..26]
+#this breaks the string down into numbers, therefore by typing 14..26 gives you "Han Solo Luke"
+puts star_wars_seven[15]
+#this array just gives you the "a" in "Han Solo"
