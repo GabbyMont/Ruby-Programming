5a7e54d4e4b927f3bab5f1f267e36dfc44765b5d
GabbyMont <gemo1236@gmail.com>
2018-02-27 01:35:22 -0500

    Writing Hashes

---
+++ Day_7.rb
@@ -0,0 +1,8 @@
+first_hash = Hash.new
+
+first_hash["first_name"] = "Gabby"
+first_hash["nick_name"] = "Gab"
+first_hash["last_name"] = "Montantez"
+
+puts first_hash["frst_name"]
+#code did not work
