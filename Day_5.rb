9b98eebaf8fd48cac3f7bcf59cfd3abb1ddcb9ba
GabbyMont <gemo1236@gmail.com>
2018-02-27 01:28:34 -0500

    .split and arrays

---
+++ Day_5.rb
@@ -0,0 +1,17 @@
+rock_bottom = "Mary Kate Olson Lindsay Lohan Charlie Sheen" #variable = "string"
+
+puts rock_bottom.split #puts each word on an individual line or creating a new line for each word
+
+date = "11/17/2013"
+
+puts date.split("/")
+
+puts date[2]
+
+puts date[0..4]#includes the 4th character
+
+puts date[0...5]#doesn't include the 5th character
+
+array_families = ["Brady", "Partridge", "Bundy"]# each string gets put on its ow line
+
+puts array_families
