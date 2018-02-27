5a03bf4b0158c3a023cbd368daffc2dbed3f04df
GabbyMont <gemo1236@gmail.com>
2018-02-27 01:45:42 -0500

    Counter, index, .each do

---
+++ day_11.rb
@@ -0,0 +1,6 @@
+favorite_candy = ["Reeses", "Snickers", "Kit Kat", "Twix"]
+
+favorite_candy.each_with_index do |item,index|
+	puts " #{index}. #{item}" #you could do a "-" or a ":" or a "."
+	
+end
