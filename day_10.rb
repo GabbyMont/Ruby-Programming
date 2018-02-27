e2b56abfc0955488604bfc415a59ad22802cef10
GabbyMont <gemo1236@gmail.com>
2018-02-27 01:39:20 -0500

    Functions

---
+++ day_10.rb
@@ -0,0 +1,14 @@
+def sagan
+ puts "Somewhere soemthing incredible is waiting to be known."
+end
+
+def osteen
+	puts "You can change the world by changing your words."
+end
+
+osteen
+sagan
+puts "i like to move it move it"
+sagan
+osteen
+puts "The watermelon crawl."
