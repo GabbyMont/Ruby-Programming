c374c41830b87f6c4cdac064e873a870cad4f98d
GabbyMont <gemo1236@gmail.com>
2018-02-27 01:27:25 -0500

    Formatting: capitalize, upcase, downcase, reverse, length

---
+++ Day_4.rb
@@ -0,0 +1,8 @@
+my_name = "gabbymontantez"
+
+puts my_name.capitalize #capitalizes the first letter of message in string
+puts my_name.upcase #makes all letters in the message in the string uppercase
+puts my_name.downcase #makes all the letters of the message in the string lowercase
+puts my_name.reverse #reverses the message in the string (zetnatnomybbag)
+puts my_name.length #gives length of message (14)
+end
