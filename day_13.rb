965576b22a03250fc988a449ee9a491ec8334fab
GabbyMont <gemo1236@gmail.com>
2018-02-27 01:51:28 -0500

    Pythagoream Theorem Calculator

---
+++ day_13.rb
@@ -0,0 +1,6 @@
+print "What is the triangle's height?"
+h = gets.chomp.to_f;
+print "What is the width of the triangle?"
+w = gets.chomp.to_f;
+area = 0.5*h*w
+puts "Guess what! Your triangle's area is #{area}!"
